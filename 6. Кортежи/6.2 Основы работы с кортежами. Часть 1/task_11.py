4a617b57f91f517cc62896213a353ae9a982ceb1
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-07-10 16:07:23 +0400

    20250710

---
+++ 6. Кортежи/6.2 Основы работы с кортежами. Часть 1/task_11.py
@@ -0,0 +1,14 @@
+'''
+Дополните приведенный ниже код так, 
+чтобы переменная index содержала индекс элемента 'Slovenia' в кортеже countries.
+'''
+
+# example
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+index = ...
+print(index)
+
+# review
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+index = countries.index('Slovenia')
+print(index)
