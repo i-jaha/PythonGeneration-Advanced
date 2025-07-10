087f180cfbe8467f06c3107064c34571be354a12
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-07-10 16:02:29 +0400

    20250710

---
+++ 6. Кортежи/6.2 Основы работы с кортежами. Часть 1/task_09.py
@@ -0,0 +1,14 @@
+'''
+Дополните приведенный ниже код так, 
+чтобы переменная number содержала количество элементов кортежа countries.
+'''
+
+# example
+countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria', 'Slovakia', 'Slovenia', 'Hungary')
+number = ...
+print(number)
+
+# review
+countries = ('Romania', 'Poland', 'Estonia', 'Bulgaria', 'Slovakia', 'Slovenia', 'Hungary')
+number = len(countries)
+print(number)
