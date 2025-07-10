8b31bde31dc6727e239d6e3ef485b7f9b153d597
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-07-10 15:47:20 +0400

    20250710

---
+++ 6. Кортежи/6.2 Основы работы с кортежами. Часть 1/task_04.py
@@ -0,0 +1,15 @@
+'''
+Используя индексацию кортежа, 
+дополните приведенный ниже код так, 
+чтобы переменная last содержала последний элемент кортежа countries.
+'''
+
+# example
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+last = ...
+print(last)
+
+# review 1
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+last = countries[-1]
+print(last)
