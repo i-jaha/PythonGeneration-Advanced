645b55ea9c62273da7b7feb9c3ae5f2dcd50c51d
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-07-10 15:51:24 +0400

    20250710

---
+++ 6. Кортежи/6.2 Основы работы с кортежами. Часть 1/task_06.py
@@ -0,0 +1,17 @@
+'''
+Используя срезы, 
+дополните приведенный ниже код так, 
+чтобы он вывел элементы кортежа countries кроме первых двух.
+
+Примечание. 
+Результатом вывода должна быть строка 
+('Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon').
+'''
+
+# example
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print()
+
+# review
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print(countries[2:])
