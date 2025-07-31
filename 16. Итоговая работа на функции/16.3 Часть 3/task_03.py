b48d1d4ae701aecfec4425b53f85bbce614c162d
Iskender Dzhakhangirli <ijahaqa@gmail.com>
2025-07-31 23:13:45 +0400

    20250731

---
+++ 16. Итоговая работа на функции/16.3 Часть 3/task_03.py
@@ -0,0 +1,17 @@
+'''
+Дан список слов words. 
+Допишите код после оператора распаковки (*), 
+который оборачивает в двойные кавычки все элементы списка words, 
+а затем печатает результат на одной строке через пробел.
+
+Примечание. 
+Вспомните про встроенную функцию map() и анонимные функции lambda.
+'''
+
+# example
+# words = 'the world is mine take a look what you have started'.split()
+# print(*...)
+
+# review
+words = 'the world is mine take a look what you have started'.split()
+print(*map(lambda x: f'"{x}"', words))
